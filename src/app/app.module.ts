c88663f107a5cb4b7bec14a317a944e166fc80a4
Kwasi Gyasi-Agyei <kwasi@edged.agency>
2018-02-12 13:54:16 +0200

    Fixed conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
 import { HomePageComponent } from './home-page/home-page.component';
-=======
 import { RegisterComponent } from './register/register.component';
->>>>>>> 21cb75899617af1bed4b264f76a49c2c69ed46c2
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-<<<<<<< HEAD
-    HomePageComponent
-=======
+    HomePageComponent,
     RegisterComponent
->>>>>>> 21cb75899617af1bed4b264f76a49c2c69ed46c2
   ],
   imports: [
     BrowserModule
