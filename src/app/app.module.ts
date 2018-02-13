b9e0ae0b72d1dd85f691e429ec15071e96c7c14b
sbusiso <sitholesbusiso.94@gmail.com>
2018-02-13 10:46:39 +0200

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,14 @@ import { LoginComponent } from './login/login.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { RegisterComponent } from './register/register.component';
 import { AboutPageComponent } from './about-page/about-page.component';
+import {RouterModule, Routes} from '@angular/router';
 
+const appRoutes: Routes = [
+  {path: 'register', component: RegisterComponent},
+  {path: 'about-page', component: AboutPageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'home-page', component: HomePageComponent},
+  ]
 
 @NgModule({
   declarations: [
@@ -17,7 +24,8 @@ import { AboutPageComponent } from './about-page/about-page.component';
    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
