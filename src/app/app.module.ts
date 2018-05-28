8a583603b7bbd98967f45472aa2f91056fb26db8
Tsundzukani <tsundzumo@gmail.com>
2018-05-28 14:40:25 +0200

    all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import {NgbModule, NgbTabsetConfig} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    NavBarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
