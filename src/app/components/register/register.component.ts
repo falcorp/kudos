92442c31bd0bc20a99fc81022f6f215d333cd2c0
desupink (Kara-BOH!) <desupink@aol.com>
2018-05-29 13:59:36 +0200

    Branch test

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {Observable} from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import {Router} from '@angular/router';
+import {FormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+    password: string;
+    email: string;
+    error: any;
 
-  ngOnInit() {
-  }
+
+    constructor(public  af: AngularFireAuth, private router: Router) {
+
+    }
+
+    onSubmit() {
+            this.af.auth.createUser({
+                email: this.email,
+                password: this.password
+            }).then(
+                (success) => {
+                    this.router.navigate(['/login']);
+                }).fail(
+                (err) => {
+                    console.log(err);
+                    this.error = err;
+                });
+
+    }
+
+    ngOnInit() {
+    }
 
 }
